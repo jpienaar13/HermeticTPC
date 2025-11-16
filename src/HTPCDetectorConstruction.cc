6fac834e00f87881a709aea72c6b5c24dc47a458
oviraptor <DIGI.I4ZJZD87@PM.ME>
2025-11-16 11:08:59 +0200

    new sync

--- src/HTPCDetectorConstruction.cc
+++ src/HTPCDetectorConstruction.cc
@@ -1056,8 +1056,8 @@ void HTPCDetectorConstruction::ConstructTPC()
     // VisAttributes
     auto col_Sapphire = G4Colour(1., 1., 0., Sapphire_Alpha);
     G4VisAttributes* vis_Sapphire = new G4VisAttributes(col_Sapphire);
-    vis_Sapphire      ->SetVisibility(true);
-    vis_Sapphire      ->SetForceSolid(true);
+    vis_Sapphire         ->SetVisibility(true);
+    vis_Sapphire         ->SetForceSolid(true);
     logic_GXeSapphireTub ->SetVisAttributes(vis_Sapphire);
     logic_LXeSapphireTub ->SetVisAttributes(vis_Sapphire);
 
@@ -1065,8 +1065,8 @@ void HTPCDetectorConstruction::ConstructTPC()
     G4VisAttributes* vis_SapphireCap = new G4VisAttributes(col_SapphireCap);
     vis_SapphireCap      ->SetVisibility(true);
     vis_SapphireCap      ->SetForceSolid(true);
-    //vis_SapphireCap ->SetForceWireframe(true);
-    vis_SapphireCap ->SetForceAuxEdgeVisible(true);
+    //vis_SapphireCap    ->SetForceWireframe(true);
+    vis_SapphireCap      ->SetForceAuxEdgeVisible(true);
     logic_GXeSapphireCap ->SetVisAttributes(vis_SapphireCap);
     logic_LXeSapphireCap ->SetVisAttributes(vis_SapphireCap);
 
@@ -1077,7 +1077,9 @@ void HTPCDetectorConstruction::ConstructTPC()
 
     // Construct Top PMT array
     G4int iNbPMTs = 1120;
-    G4double dPMTOffsetZTop = -(iCryostat_H/2) * (1-LiquidGasRatio) + GXeTeflonTub_H + dPMTHeight/2;
+    G4double dPMTOffsetZTop = -(iCryostat_H/2) * (1-LiquidGasRatio) 
+                              + GXeTeflonTub_H 
+                              + dPMTHeight/2;
 
     m_pPmtR11410LogicalVolume = ConstructPMT();
     for (G4int iPMT = 0; iPMT < iNbPMTs; ++iPMT) {
@@ -1091,7 +1093,10 @@ void HTPCDetectorConstruction::ConstructTPC()
     }
 
     // Construct Bottom PMT array
-    G4double dPMTOffsetZBot = (iCryostat_H/2) * LiquidGasRatio - LXeTeflonTub_H - dPMTHeight/2;
+    G4double dPMTOffsetZBot = (iCryostat_H/2) * LiquidGasRatio 
+                              - LXeTeflonTub_H 
+                              - dPMTHeight/2;
+    
     G4RotationMatrix *pRotX180 = new G4RotationMatrix();
     pRotX180->rotateX(180. * deg);
 
@@ -1116,44 +1121,44 @@ void HTPCDetectorConstruction::ConstructTPC()
 
 G4LogicalVolume *HTPCDetectorConstruction::ConstructPMT() {
 
-  G4double dPMTOuterRadius = 38. * mm;
+  G4double dPMTOuterRadius      = 38. * mm;
   G4double dPMTOuterRingBottomZ = 9. * mm;
-  G4double dPMTOuterRingHeight = 5. * mm;
-  G4double dPMTOuterRingRadius = 38.75 * mm;
+  G4double dPMTOuterRingHeight  = 5. * mm;
+  G4double dPMTOuterRingRadius  = 38.75 * mm;
 
-  G4double dPMTWindowRadius = 35. * mm;
+  G4double dPMTWindowRadius    = 35. * mm;
   G4double dPMTWindowThickness = 3.5 * mm;
-  G4double dPMTWindowGap = 0.6 * mm;
+  G4double dPMTWindowGap       = 0.6 * mm;
 
-  G4double dPMTThickness = 1. * mm;
-  G4double dPMTHeight = 114. * mm;
+  G4double dPMTThickness  = 1. * mm;
+  G4double dPMTHeight     = 114. * mm;
   G4double dPMTDinodeCutZ = 11.8 * mm;
   G4double dPMTFunnelCutZ = 11.8 * mm;
   G4double dPMTBaseRadius = 26.65 * mm;
 
-  G4double dPMTPhotocathodeRadius = 32. * mm;
+  G4double dPMTPhotocathodeRadius    = 32. * mm;
   G4double dPMTPhotocathodeThickness = 0.1 * mm;
 
-  G4double dPMTCeramicRadius = dPMTBaseRadius - 5. * mm;
+  G4double dPMTCeramicRadius    = dPMTBaseRadius - 5. * mm;
   G4double dPMTCeramicThickness = 4. * mm;
 
   G4Material *Quartz = G4Material::GetMaterial("Quartz");
-  G4Material *Kovar = G4Material::GetMaterial("Kovar");
+  G4Material *Kovar  = G4Material::GetMaterial("Kovar");
   G4Material *Vacuum = G4Material::GetMaterial("Vacuum");
   G4Material *PhotoCathodeAluminium = G4Material::GetMaterial("PhotoCathodeAluminium");
   G4Material *Ceramic = G4Material::GetMaterial("Ceramic");
 
   //------------------------- PMT body ---------------------
-  const G4double dPMTZ0 = -0.5 * dPMTHeight;
-  const G4double dPMTZ1 = dPMTZ0 + dPMTOuterRingBottomZ;
-  const G4double dPMTZ2 = dPMTZ1;
-  const G4double dPMTZ3 = dPMTZ2 + dPMTOuterRingHeight;
-  const G4double dPMTZ4 = dPMTZ3;
-  const G4double dPMTZ5 = dPMTZ4 + dPMTDinodeCutZ;
-  const G4double dPMTZ6 = dPMTZ5 + dPMTFunnelCutZ;
-  const G4double dPMTZ7 = dPMTZ6 + 1. * cm;
-  const G4double dPMTZ8 = dPMTZ7;
-  const G4double dPMTZ9 = dPMTZ8 + 1. * mm;
+  const G4double dPMTZ0  = -0.5 * dPMTHeight;
+  const G4double dPMTZ1  = dPMTZ0 + dPMTOuterRingBottomZ;
+  const G4double dPMTZ2  = dPMTZ1;
+  const G4double dPMTZ3  = dPMTZ2 + dPMTOuterRingHeight;
+  const G4double dPMTZ4  = dPMTZ3;
+  const G4double dPMTZ5  = dPMTZ4 + dPMTDinodeCutZ;
+  const G4double dPMTZ6  = dPMTZ5 + dPMTFunnelCutZ;
+  const G4double dPMTZ7  = dPMTZ6 + 1. * cm;
+  const G4double dPMTZ8  = dPMTZ7;
+  const G4double dPMTZ9  = dPMTZ8 + 1. * mm;
   const G4double dPMTZ10 = dPMTZ9;
   const G4double dPMTZ11 = 0.5 * dPMTHeight - 6. * mm;
   const G4double dPMTZ12 = 0.5 * dPMTHeight - 6. * mm;
@@ -1161,16 +1166,16 @@ G4LogicalVolume *HTPCDetectorConstruction::ConstructPMT() {
   const G4double dPMTZ14 = 0.5 * dPMTHeight - 5. * mm;
   const G4double dPMTZ15 = 0.5 * dPMTHeight;
 
-  const G4double dPMTR0 = dPMTOuterRadius;
-  const G4double dPMTR1 = dPMTR0;
-  const G4double dPMTR2 = dPMTOuterRingRadius;
-  const G4double dPMTR3 = dPMTR2;
-  const G4double dPMTR4 = dPMTR0;
-  const G4double dPMTR5 = dPMTR0;
-  const G4double dPMTR6 = dPMTBaseRadius;
-  const G4double dPMTR7 = dPMTR6;
-  const G4double dPMTR8 = dPMTR6 + 0.5 * mm;
-  const G4double dPMTR9 = dPMTR6 + 0.5 * mm;
+  const G4double dPMTR0  = dPMTOuterRadius;
+  const G4double dPMTR1  = dPMTR0;
+  const G4double dPMTR2  = dPMTOuterRingRadius;
+  const G4double dPMTR3  = dPMTR2;
+  const G4double dPMTR4  = dPMTR0;
+  const G4double dPMTR5  = dPMTR0;
+  const G4double dPMTR6  = dPMTBaseRadius;
+  const G4double dPMTR7  = dPMTR6;
+  const G4double dPMTR8  = dPMTR6 + 0.5 * mm;
+  const G4double dPMTR9  = dPMTR6 + 0.5 * mm;
   const G4double dPMTR10 = dPMTR6;
   const G4double dPMTR11 = dPMTR6;
   const G4double dPMTR12 = dPMTR6 + 0.5 * mm;
@@ -1187,26 +1192,58 @@ G4LogicalVolume *HTPCDetectorConstruction::ConstructPMT() {
       dPMTR0, dPMTR1, dPMTR2,  dPMTR3,  dPMTR4,  dPMTR5,  dPMTR6,  dPMTR7,
       dPMTR8, dPMTR9, dPMTR10, dPMTR11, dPMTR12, dPMTR13, dPMTR14, dPMTR15};
 
-  G4Polycone *pPMTPolycone = new G4Polycone("pPMTPolycone", 0., 2 * M_PI, 16,
-                                            dPMTZPlane, dPMTInner, dPMTOuter);
-  G4Tubs *pPmtCut1Tubs =
-      new G4Tubs("pPmtCut1Tubs", dPMTWindowRadius, dPMTOuterRadius,
-                 dPMTWindowGap, 0., 2 * M_PI);
+  G4Polycone *pPMTPolycone = new G4Polycone(
+    "pPMTPolycone", 
+    0., 
+    2 * M_PI, 
+    16,
+    dPMTZPlane, 
+    dPMTInner, 
+    dPMTOuter
+    );
 
-  G4SubtractionSolid *pPmtSubtractionSolid =
-      new G4SubtractionSolid("pPmtSubtractionSolid1", pPMTPolycone,
-                             pPmtCut1Tubs, 0, G4ThreeVector(0., 0., dPMTZ0));
+  G4Tubs *pPmtCut1Tubs = new G4Tubs(
+    "pPmtCut1Tubs", 
+    dPMTWindowRadius, 
+    dPMTOuterRadius,
+    dPMTWindowGap, 
+    0., 
+    2 * M_PI
+    );
+
+  G4SubtractionSolid *pPmtSubtractionSolid = new G4SubtractionSolid(
+    "pPmtSubtractionSolid1", 
+    pPMTPolycone,
+    pPmtCut1Tubs, 
+    0, 
+    G4ThreeVector(0., 0., dPMTZ0)
+    );
 
   G4Tubs *pPmtCut2Tubs = new G4Tubs(
-      "pPmtCut2Tubs", dPMTWindowRadius, dPMTOuterRadius - dPMTThickness,
-      dPMTWindowThickness * 0.5 - dPMTThickness * 0.5, 0., 2 * M_PI);
+      "pPmtCut2Tubs", 
+      dPMTWindowRadius, 
+      dPMTOuterRadius - dPMTThickness,
+      dPMTWindowThickness * 0.5 - dPMTThickness * 0.5, 
+      0., 
+      2 * M_PI
+    );
 
   pPmtSubtractionSolid = new G4SubtractionSolid(
-      "pPmtSubtractionSolid2", pPmtSubtractionSolid, pPmtCut2Tubs, 0,
-      G4ThreeVector(0., 0., dPMTZ0 + dPMTWindowThickness * 0.5));
+      "pPmtSubtractionSolid2", 
+      pPmtSubtractionSolid, 
+      pPmtCut2Tubs, 
+      0,
+      G4ThreeVector(0., 0., dPMTZ0 + dPMTWindowThickness * 0.5)
+    );
 
-  m_pPMTLogicalVolume = new G4LogicalVolume(pPmtSubtractionSolid, Kovar,
-                                            "PMTLogicalVolume", 0, 0, 0);
+  m_pPMTLogicalVolume = new G4LogicalVolume(
+    pPmtSubtractionSolid, 
+    Kovar,
+    "PMTLogicalVolume", 
+    0, 
+    0, 
+    0
+    );
 
   //------------------------ Inner Vacuum -----------------------
   const G4double dPMTCutZ0 = 0. * cm;
@@ -1227,70 +1264,159 @@ G4LogicalVolume *HTPCDetectorConstruction::ConstructPMT() {
   const G4double dPMTCutInner[] = {0., 0., 0., 0.};
   const G4double dPMTCutOuter[] = {dPMTCutR0, dPMTCutR1, dPMTCutR2, dPMTCutR3};
 
-  G4Polycone *pPMTInnerVacuumPolycone =
-      new G4Polycone("pPMTInnerVacuumPolycone", 0. * deg, 360. * deg, 4,
-                     dPMTZCutPlane, dPMTCutInner, dPMTCutOuter);
+  G4Polycone *pPMTInnerVacuumPolycone = new G4Polycone(
+    "pPMTInnerVacuumPolycone", 
+    0. * deg, 
+    360. * deg, 
+    4,
+    dPMTZCutPlane, 
+    dPMTCutInner, 
+    dPMTCutOuter
+    );
 
-  G4Tubs *pPMTVacuumCut =
-      new G4Tubs("pPMTVacuumCut", dPMTPhotocathodeRadius, dPMTOuterRadius,
-                 dPMTThickness * 0.5, 0. * deg, 360. * deg);
+  G4Tubs *pPMTVacuumCut = new G4Tubs(
+    "pPMTVacuumCut", 
+    dPMTPhotocathodeRadius, 
+    dPMTOuterRadius,
+    dPMTThickness * 0.5, 
+    0. * deg, 
+    360. * deg
+    );
 
   G4SubtractionSolid *pPMTVacuumSubtractionSolid = new G4SubtractionSolid(
-      "pPMTVacuumSubtractionSolid", pPMTInnerVacuumPolycone, pPMTVacuumCut, 0,
-      G4ThreeVector(0., 0., dPMTCutZ0 + dPMTThickness * 0.5));
+      "pPMTVacuumSubtractionSolid", 
+      pPMTInnerVacuumPolycone, 
+      pPMTVacuumCut, 
+      0,
+      G4ThreeVector(0., 0., dPMTCutZ0 + dPMTThickness * 0.5)
+    );
 
-  G4Tubs *pCut = new G4Tubs("pPMTCut", 0. * mm, dPMTCeramicRadius,
-                            dPMTCeramicThickness * 0.5, 0. * deg, 360. * deg);
+  G4Tubs *pCut = new G4Tubs(
+    "pPMTCut", 
+    0. * mm, 
+    dPMTCeramicRadius,
+    dPMTCeramicThickness * 0.5, 
+    0. * deg, 
+    60. * deg
+    );
+
   G4SubtractionSolid *pPMTcut1 = new G4SubtractionSolid(
-      "pPMTVacuumSubtractionSolid", pPMTVacuumSubtractionSolid, pCut, 0,
+      "pPMTVacuumSubtractionSolid", 
+      pPMTVacuumSubtractionSolid, 
+      pCut, 
+      0,
       G4ThreeVector(0., 0.,
-                    dPMTCutZ3 + dPMTThickness - dPMTCeramicThickness * 0.5));
+                    dPMTCutZ3 + dPMTThickness - dPMTCeramicThickness * 0.5)
+    );
 
   m_pPMTInnerVacuumLogicalVolume = new G4LogicalVolume(
-      pPMTcut1, Vacuum, "PMTInnerVacuumLogicalVolume", 0, 0, 0);
+      pPMTcut1, 
+      Vacuum, 
+      "PMTInnerVacuumLogicalVolume", 
+      0, 
+      0, 
+      0
+    );
 
-  m_pPMTInnerVacuumPhysicalVolume =
-      new G4PVPlacement(0, G4ThreeVector(0, 0, dPMTInnerVacuumOffsetZ),
-                        m_pPMTInnerVacuumLogicalVolume, "PMTInnerVacuum",
-                        m_pPMTLogicalVolume, false, 0);
+  m_pPMTInnerVacuumPhysicalVolume = new G4PVPlacement(
+    0, 
+    G4ThreeVector(0, 0, dPMTInnerVacuumOffsetZ),
+    m_pPMTInnerVacuumLogicalVolume, 
+    "PMTInnerVacuum",
+    m_pPMTLogicalVolume, 
+    false, 
+    0
+    );
 
   //------------------------ Quartz window -----------------------
   const G4double dPMTWindowOffsetZ = 0.5 * (dPMTWindowThickness - dPMTHeight);
 
-  G4Tubs *pPMTWindow =
-      new G4Tubs("pPMTWindow", 0. * mm, dPMTWindowRadius,
-                 dPMTWindowThickness * 0.5, 0. * deg, 360. * deg);
+  G4Tubs *pPMTWindow = new G4Tubs(
+    "pPMTWindow", 
+    0. * mm, 
+    dPMTWindowRadius,
+    dPMTWindowThickness * 0.5, 
+    0. * deg, 
+    360. * deg
+    );
+
   m_pPMTWindowLogicalVolume = new G4LogicalVolume(
-      pPMTWindow, Quartz, "PMTWindowLogicalVolume", 0, 0, 0);
+      pPMTWindow, 
+      Quartz, 
+      "PMTWindowLogicalVolume", 
+      0, 
+      0, 
+      0
+    );
+
   m_pPMTWindowPhysicalVolume = new G4PVPlacement(
-      0, G4ThreeVector(0, 0, dPMTWindowOffsetZ), m_pPMTWindowLogicalVolume,
-      "Quartz_PMTWindow", m_pPMTLogicalVolume, false, 0);
+      0, 
+      G4ThreeVector(0, 0, dPMTWindowOffsetZ), m_pPMTWindowLogicalVolume,
+      "Quartz_PMTWindow", 
+      m_pPMTLogicalVolume, 
+      false, 
+      0
+    );
 
   //------------------------ Photocathode ------------------------
-  G4Tubs *pPMTPhotocathode =
-      new G4Tubs("pPMTPhotocathode", 0. * mm, dPMTPhotocathodeRadius,
-                 dPMTPhotocathodeThickness * 0.5, 0. * deg, 360. * deg);
-  m_pPMTPhotocathodeLogicalVolume =
-      new G4LogicalVolume(pPMTPhotocathode, PhotoCathodeAluminium,
-                          "PMTPhotocathodeLogicalVolume", 0, 0, 0);
-  m_pPMTPhotocathodePhysicalVolume = new G4PVPlacement(
-      0, G4ThreeVector(0, 0, dPMTCutZ0 + dPMTPhotocathodeThickness * 0.5),
-      m_pPMTPhotocathodeLogicalVolume, "PMTPhotocathode",
-      m_pPMTInnerVacuumLogicalVolume, false, 0);
+  G4Tubs *pPMTPhotocathode = new G4Tubs(
+    "pPMTPhotocathode", 
+    0. * mm, 
+    dPMTPhotocathodeRadius,
+    dPMTPhotocathodeThickness * 0.5, 
+    0. * deg, 
+    360. * deg
+    );
 
+  m_pPMTPhotocathodeLogicalVolume = new G4LogicalVolume(
+    pPMTPhotocathode, 
+    PhotoCathodeAluminium,
+    "PMTPhotocathodeLogicalVolume", 
+    0, 
+    0, 
+    0
+    );
+
+  m_pPMTPhotocathodePhysicalVolume = new G4PVPlacement(
+      0, 
+      G4ThreeVector(0, 0, dPMTCutZ0 + dPMTPhotocathodeThickness * 0.5),
+      m_pPMTPhotocathodeLogicalVolume, 
+      "PMTPhotocathode",
+      m_pPMTInnerVacuumLogicalVolume, 
+      false, 
+      0
+    );
 
   //------------------------ PMTs Ceramic Stem ------------------------
   G4double dCeramicOffsetZ = (dPMTHeight - dPMTCeramicThickness) * 0.5;
 
-  G4Tubs *pPMTCeramic =
-      new G4Tubs("pPMTCeramic", 0. * mm, dPMTCeramicRadius,
-                 dPMTCeramicThickness * 0.5, 0. * deg, 360. * deg);
-  m_PMTCeramicLogicalVolume = new G4LogicalVolume(
-      pPMTCeramic, Ceramic, "PMTCeramicLogicalVolume", 0, 0, 0);
-  m_PMTCeramicPhysicalVolume = new G4PVPlacement(
-      0, G4ThreeVector(0, 0, dCeramicOffsetZ), m_PMTCeramicLogicalVolume,
-      "Ceramic_PMTStem", m_pPMTLogicalVolume, false, 0);
+  G4Tubs *pPMTCeramic = new G4Tubs(
+    "pPMTCeramic", 
+    0. * mm, 
+    dPMTCeramicRadius,
+    dPMTCeramicThickness * 0.5, 
+    0. * deg, 
+    360. * deg
+    );
 
+  m_PMTCeramicLogicalVolume = new G4LogicalVolume(
+      pPMTCeramic, 
+      Ceramic, 
+      "PMTCeramicLogicalVolume", 
+      0, 
+      0, 
+      0
+    );
+
+  m_PMTCeramicPhysicalVolume = new G4PVPlacement(
+      0, 
+      G4ThreeVector(0, 0, dCeramicOffsetZ), 
+      m_PMTCeramicLogicalVolume,
+      "Ceramic_PMTStem", 
+      m_pPMTLogicalVolume, 
+      false, 
+      0
+    );
 
   //---------------------------------- attributes
 
