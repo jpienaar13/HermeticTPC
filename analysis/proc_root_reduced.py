b0c77649f87f4497aa58cb1a75d22ca7652de1cc
oviraptor <DIGI.I4ZJZD87@PM.ME>
2025-11-30 09:58:10 +0200

    Updated proc_root_reduced to include energy of initiating gamma and number of clusters

--- analysis/proc_root_reduced.py
+++ analysis/proc_root_reduced.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
 import uproot
+import math
 import numpy as np
 from numpy import ndarray
 import pandas as pd
 from tqdm import tqdm
 import argparse
 
-
-def calculate_norm_distance(tup1, tup2):
+def calculate_norm_distance(
+        tup1 : tuple, 
+        tup2 : tuple
+        )   -> float:
+    
     x1, y1, z1 = tup1
     x2, y2, z2 = tup2
 
@@ -16,8 +20,16 @@ def calculate_norm_distance(tup1, tup2):
     sqy = (y2 - y1)**2
     sqz = (z2 - z1)**2
     
-    distance = np.sqrt(sqx + sqy + sqz)
+    distance = math.sqrt(sqx + sqy + sqz)
     return distance
+
+
+def calculate_z_distance(
+        z1 : float, 
+        z2 : float
+        )   -> float:
+    
+    return abs(z2 - z1)
 
 
 def generate_cluster_matrix(
@@ -33,12 +45,10 @@ def generate_cluster_matrix(
 
     cluster_matrix = []
 
-    cluster_arr = [idxs[0]]
-    prev_coord = (0, 0, z_arr[idxs[0]])
-    #prev_coord = (x_arr[idxs[0]], y_arr[idxs[0]], z_arr[idxs[0]])
+    cluster_arr = [idxs[0]] 
+    prev_coord = (x_arr[idxs[0]], y_arr[idxs[0]], z_arr[idxs[0]])
     for i in idxs[1:]:
-        coord = (0, 0, z_arr[i])
-        #coord = (x_arr[i], y_arr[i], z_arr[i])
+        coord = (x_arr[i], y_arr[i], z_arr[i])
         distance = calculate_norm_distance(prev_coord, coord)
         prev_coord = coord
         if distance < scale:
@@ -51,14 +61,35 @@ def generate_cluster_matrix(
     
     return cluster_matrix
 
-
-def read_root_file(fn, treename='events', istart=0, istop=1000):
+def read_root_file(
+        fn,
+        treename='events', 
+        istart=0, 
+        istop=1000
+        ):
 
     with uproot.open(fn) as f:
         tree = f[f"{treename}"][f"{treename}"]
-        branches = ['nsteps', 'xp', 'yp', 'zp', 'etot', 'xp_pri', 'yp_pri', 'zp_pri', 'ed', 'time']
-        data = tree.arrays(branches, filter_name='nsteps', library='np', entry_start=istart, entry_stop=istop)
-        total = len(tree.arrays('etot'))
+        branches = ['nsteps', 
+                    'xp', 
+                    'yp', 
+                    'zp', 
+                    'etot', 
+                    'xp_pri', 
+                    'yp_pri', 
+                    'zp_pri', 
+                    'ed',
+                    'PreStepEnergy',
+                    'type',
+                    'time']
+        
+        data = tree.arrays(
+            branches, 
+            filter_name='nsteps', 
+            library='np', 
+            entry_start=istart, 
+            entry_stop=istop
+            )
 
     return data
 
@@ -73,33 +104,48 @@ def get_total_events(fn, treename='events'):
 def generate_reduced_df(data, scale=10):
 
     df = pd.DataFrame(data)
-    N = len(df)
+    df = df[df['etot'] > 0] # Select only events with energy depositions    
+    length = len(df)
     
-    Clusters_main = []
-    E_main = []
-    xp_main = []
-    yp_main = []
-    zp_main = []
-    xp_pr_main = []
-    yp_pr_main = []
-    zp_pr_main = []
+    Clusters_main = [] #vector
+    Clusterval_main = [] # scalar
+    EG_main = [] #scalar
+    Edep_main = [] #vector
+    xp_main = [] #vector
+    yp_main = [] #vector
+    zp_main = [] #vector
+    xp_pr_main = [] #scalar
+    yp_pr_main = [] #scalar
+    zp_pr_main = [] #scalar
     
-    
-    for idx in range(N):
+    for idx in range(length):
         try:
-            xp_pr = float(df.loc[:, 'xp_pri'][idx])
-            yp_pr = float(df.loc[:, 'yp_pri'][idx])
-            zp_pr = float(df.loc[:, 'zp_pri'][idx])
+            event_df = df.iloc[idx]
+
+            mask = event_df['type'] == 'gamma'
+            energies = event_df['PreStepEnergy'][mask]
+            e_gam = float(energies[0])
+
+            xp_pr = float(event_df['xp_pri'])
+            yp_pr = float(event_df['yp_pri'])
+            zp_pr = float(event_df['zp_pri'])
             
-            z_arr = np.array(df.loc[:, 'zp'][idx])
-            x_arr = np.array(df.loc[:, 'xp'][idx])
-            y_arr = np.array(df.loc[:, 'yp'][idx])
-            e_arr = np.array(df.loc[:, 'ed'][idx])
-            cluster_matrix = generate_cluster_matrix(x_arr, y_arr, z_arr, e_arr, scale)
+            z_arr = np.array(event_df['zp'])
+            x_arr = np.array(event_df['xp'])
+            y_arr = np.array(event_df['yp'])
+            ed_arr = np.array(event_df['ed'])
+
+            cluster_matrix = generate_cluster_matrix(
+                x_arr, 
+                y_arr, 
+                z_arr, 
+                ed_arr, 
+                scale
+                )
     
             sz = len(cluster_matrix)
     
-            E_vals  = []
+            Edep_vals  = []
             X_vals  = []
             Y_vals  = []
             Z_vals  = []
@@ -108,21 +154,23 @@ def generate_reduced_df(data, scale=10):
                 x = x_arr[cluster]
                 y = y_arr[cluster]
                 z = z_arr[cluster]
-                e = e_arr[cluster]
+                ed = ed_arr[cluster]
     
-                E = np.nansum(e)
+                E = np.nansum(ed)
                 X = np.nanmean(x)
                 Y = np.nanmean(y)
                 Z = np.nanmean(z)
     
-                E_vals.append(E)
+                Edep_vals.append(E)
                 X_vals.append(X)
                 Y_vals.append(Y)
                 Z_vals.append(Z)
         
     
             Clusters_main.append(sz)
-            E_main.append(np.array(E_vals))
+            Clusterval_main.append(sz)
+            EG_main.append(e_gam)
+            Edep_main.append(np.array(Edep_vals))
             xp_main.append(np.array(X_vals))
             yp_main.append(np.array(Y_vals))
             zp_main.append(np.array(Z_vals))
@@ -135,13 +183,15 @@ def generate_reduced_df(data, scale=10):
     
     data_reduced = {
         "clusters" : np.array(Clusters_main),
-        "edep"     : E_main,
+        "nclusters" : Clusterval_main,
+        "edep"     : Edep_main,
         "xp"       : xp_main,
         "yp"       : yp_main,
         "zp"       : zp_main,
-        "xp_pri"    : xp_pr_main,
-        "yp_pri"    : yp_pr_main,
-        "zp_pri"    : zp_pr_main
+        "e_gam"   : EG_main,
+        "xp_pri"   : xp_pr_main,
+        "yp_pri"   : yp_pr_main,
+        "zp_pri"   : zp_pr_main
     }
     
     df_reduced = pd.DataFrame(data_reduced)
@@ -150,8 +200,8 @@ def generate_reduced_df(data, scale=10):
 
 
 row_length = ('clusters')
-scalar_fields = ['xp_pri', 'yp_pri', 'zp_pri']
-vector_fields = ['xp', 'yp', 'zp', 'edep', ]
+scalar_fields = ['nclusters', 'e_gam', 'xp_pri', 'yp_pri', 'zp_pri']
+vector_fields = ['edep','xp', 'yp', 'zp']
 
 def flatten_mc_tree(df, last_event=0, row_length=row_length, vector_fields=vector_fields, scalar_fields=scalar_fields):
     flattened_data = []
@@ -179,9 +229,9 @@ def flatten_mc_tree(df, last_event=0, row_length=row_length, vector_fields=vecto
 
 
 # Main Processing Function
-def process_root_file(fn, treename='events', chunksize=1000, scale=10):
+def process_root_file(fn, treename='events', chunksize=1000, scale=10, fulfill = 1):
     
-    total = get_total_events(fn, treename=treename)
+    total = int(get_total_events(fn, treename=treename) * fulfill)
     df_arr = []
     prev_df = None
     for i_chunk in tqdm(range(0, total, chunksize)):
@@ -214,13 +264,16 @@ def main():
     parser.add_argument("--istop", type=int, default=-1, help="Stop processing after this many entries.")
     parser.add_argument("--outfile", type=str, default="output.parquet", help="Output Parquet file name.")
     parser.add_argument("--scale", type=float, default=10.0, help="Clustering scale in units consistent with z position.")
+    parser.add_argument("--fulfill", type=float, default=1.0, help="Fraction of total events to process.")
+
 
     args = parser.parse_args()
 
     result_df = process_root_file(
         args.infile_name,
         chunksize=args.chunksize,
-        scale=args.scale
+        scale=args.scale,
+        fulfill=args.fulfill
     )
 
     print(f"Saving DataFrame as parquet: {args.outfile}")
