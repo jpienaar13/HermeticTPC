2b005c455a44e9845f2c051e7aa135120c5edae1
Nadav Hargittai <nadavh@wipp-an1.weizmann.ac.il>
2026-01-08 20:13:11 +0200

    job submission script

--- job_submission/run_hermeticTPCsingle.sh
+++ job_submission/run_hermeticTPCsingle.sh
@@ -6,7 +6,10 @@
 #PBS -l select=1:ncpus=1:mem=2500mb
 #PBS -l io=1
 #PBS -j oe
-#PBS -o /srv01/xenon/{{USER}}/HermeticTPC/logs/{{BASENAME}}.log
+#PBS -o /storage/xenon/{{USER}}/hermeticTPC/BigStats_01/log/{{BASENAME}}.log
+
+COMPONENT=$"Sapphire"
+ISOTOPE=$"Co60"
 
 # Load environment
 echo "Activating LCG environment..."
@@ -15,10 +18,22 @@ source /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-el9-gcc12-opt/setup.sh
 if [ "{{STEP}}" != "post" ]; then
     echo "Running GEANT4 simulation..."    
     cd /srv01/xenon/{{USER}}/HermeticTPC
-    ./build/bin/hermeticTPC -f macros/{{MACROFILE}} -n {{NEVENTS}} -o /storage/xenon/{{USER}}/hermeticTPC/{{OUTFILE}}
+    ./build/bin/hermeticTPC -f macros/{{MACROFILE}} -n {{NEVENTS}} -o /storage/xenon/{{USER}}/hermeticTPC/BigStats_01/${COMPONENT}/${ISOTOPE}/{{OUTFILE}}
 
 fi
 
+# Wait for root file to show up...
+sleep 2
+
 # Post-process
-echo "Running post-processing..."
-python /srv01/xenon/{{USER}}/HermeticTPC/analysis/proc_root_reduced.py /storage/xenon/{{USER}}/hermeticTPC/{{OUTFILE}} --outfile /storage/xenon/{{USER}}/hermeticTPC/{{BASENAME}}.parquet --scale {{SCALE}}
+echo "Running post-processing: scale=10mm"
+python /srv01/xenon/{{USER}}/HermeticTPC/analysis/proc_root_reduced.py /storage/xenon/{{USER}}/hermeticTPC/BigStats_01/${COMPONENT}/${ISOTOPE}/{{OUTFILE}} --outfile /storage/xenon/{{USER}}/hermeticTPC/BigStats_01/${COMPONENT}/${ISOTOPE}/{{BASENAME}}_s10mm.parquet --scale 10
+
+echo "Running post-processing: scale=1mm"
+python /srv01/xenon/{{USER}}/HermeticTPC/analysis/proc_root_reduced.py /storage/xenon/{{USER}}/hermeticTPC/BigStats_01/${COMPONENT}/${ISOTOPE}/{{OUTFILE}} --outfile /storage/xenon/{{USER}}/hermeticTPC/BigStats_01/${COMPONENT}/${ISOTOPE}/{{BASENAME}}_s1mm.parquet --scale 1
+
+# Wait for files to stabilise
+sleep 1
+
+echo "Discarding root file"
+rm /storage/xenon/{{USER}}/hermeticTPC/BigStats_01/${COMPONENT}/${ISOTOPE}/{{BASENAME}}.root
